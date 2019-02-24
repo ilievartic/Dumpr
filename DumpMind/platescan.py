110673eb16001ba704a3cd66baac6e42dc15a4b2
Vassily Petrov <git@vassily.org>
2019-02-24 01:38:17 -0600

    improved data output

--- DumpMind/platescan.py
+++ DumpMind/platescan.py
@@ -1,31 +1,41 @@
+#!/usr/bin/python
 import cv2
 import time
 import json
 import subprocess
+import serial
 
-cam = cv2.VideoCapture(1)
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
+cam = cv2.VideoCapture(0)
 cam.set(3,1920)
 cam.set(4,1080)
 #cam.set(5,10)
 
-img_counter = 0
-
 while True:
     start = time.time()
-    ret, frame = cam.read()
-    ret, frame = cam.read()
-    ret, frame = cam.read()
-    ret, frame = cam.read()
-    ret, frame = cam.read()
+    for i in range(7):
+        # empty the framebuffer
+        ret, frame = cam.read()
     if ret:
         cv2.imwrite("/tmp/plate.jpg", frame)
         
         process = subprocess.Popen(['alpr', '-c', 'us', '-j', '/tmp/plate.jpg'], stdout=subprocess.PIPE)
         out, err = process.communicate()
         data = json.loads(out)
+        stop =  time.time() - start
         if ( len(data["results"]) > 0 and data["results"][0]["confidence"] > 85.0 ):
-            print(data["results"][0]["plate"]+"  "+str(data["results"][0]["confidence"]))
-    stop =  time.time() - start
+            print "%s conf:%.2f cycletime:%.2f" % (data["results"][0]["plate"], data["results"][0]["confidence"], stop)
+            out = "'%s'" % data["results"][0]["plate"]
+            ser.write(out.encode())
+    
     time.sleep(0.1)
 
 cam.release()
